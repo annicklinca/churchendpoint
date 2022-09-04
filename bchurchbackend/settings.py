e50d4a50a9e76f045b1964515a557d429477b5a3
Telesphore <tuganimana01@gmail.comm>
2022-09-05 00:58:15 +0200

    added cors header

--- bchurchbackend/settings.py
+++ bchurchbackend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
 ]
 
 ROOT_URLCONF = 'bchurchbackend.urls'
@@ -68,6 +71,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 WSGI_APPLICATION = 'bchurchbackend.wsgi.application'
 REST_FRAMEWORK = {
